0ed8d502c1c1715796ad35ff0fe4527e8b15dd1d
Nemleck <clement.malfreyt@gmail.com>
2023-07-05 19:32:55 +0200

    Merged all into the player file and added music

--- main.py
+++ main.py
@@ -2,24 +2,42 @@ import pyglet
 from classes.player import *
 from classes.functions import *
 from classes.gameobject import *
+from classes.functions import *
+
+# import options
+from json import loads
+with open("options.json", "r", encoding="utf-8") as f:
+    data = loads(f.read())
+
+with open("map.json", "r", encoding="utf-8") as f:
+    map = loads(f.read())
 
 # Create a window and a moveable surface
-win_width = 800
-win_height = 600
+win_width = data["window"]["width"]
+win_height = data["window"]["height"]
 window = pyglet.window.Window(win_width, win_height, "game")
 
 # Create a player
-player = Player()
+player = Player(data["player"]["starting_pos"])
 
 # Create gameobject list
 gameobjects = []
-hook1 = GameObject("hook", 39, 72, 900, 350)
-hook2 = GameObject("hook", 39, 72, 400, 300)
-gameobjects.append(hook1)
-gameobjects.append(hook2)
+for json_go_data in map:
+    gameobjects.append(
+        GameObject(
+            json_go_data["type"],
+            json_go_data["width"],
+            json_go_data["height"],
+            json_go_data["x"],
+            json_go_data["y"],
+            json_go_data["special_ability"]
+        )
+    )
+
+# sort all gamobjects based on y
+gameobjects = [elm for elm in sort_gameobjects(gameobjects)] # create copy of the sorted list
 
 # labels init
-
 coords_label = pyglet.text.Label('[0,0]',
     font_name='Impact',
     font_size=15,
@@ -36,54 +54,23 @@ rewind_labed = pyglet.text.Label('0',
 
 rewind_labed.color = (0,0,0,255)
 
-# Create plateform
-plateform1 = GameObject("grass", 1090, 1550, 0, -1550)
-plateform3 = GameObject("grass", 1090, 1550, 890, -1350)
-plateform4 = GameObject("grass", 1090, 1550, -890, -1350)
-plateform2 = GameObject("rock", 412, 92, 340, 125, special_ability=50)
-gameobjects.append(plateform1)
-gameobjects.append(plateform2)
-gameobjects.append(plateform3)
-gameobjects.append(plateform4)
-
-# sort all gamobjects based on y
-result = []
-for gameobject in gameobjects:
-    found = False
-    for i in range(len(result)):
-        if result[i].pos[1] < gameobject.pos[1]:
-            result.insert(i, gameobject)
-            found = True
-            break
-    
-    if not found:
-        result.append(gameobject)
-
-gameobjects = [elm for elm in result] # create copy of the sorted list
+# Put ground music
+ground_music = pyglet.media.load(data["music"]["location"], streaming=False)
 
 # Setup background
-bg_img = pyglet.image.load("sources/plateform/background.png")
-bg_sprite = pyglet.sprite.Sprite(bg_img, 0, 0)
-bg_sprite.scale_x = win_width
-bg_sprite.scale_y = win_height
-
-clouds_img = pyglet.image.load("sources/plateform/clouds.png")
-clouds_sprite = pyglet.sprite.Sprite(clouds_img, 0, 0)
-clouds_sprite.scale = 2
-gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
+bg_sprite, clouds_sprite = background_setup(win_width, win_height)
 
 # Var init
 scrolling = [0, 0]
 bg_x_scrolling = 0
-foundGround = False
 
 # Setup Keyboard
 keys = pyglet.window.key.KeyStateHandler()
 window.push_handlers(keys)
 
 def render_update(dt):
-    global scrolling, bg_x_scrolling
-
+    # Make clouds move
+    global bg_x_scrolling
     bg_x_scrolling += 5*dt
 
     window.clear()
@@ -117,86 +104,8 @@ def render_update(dt):
         rewind_labed.draw()
 
 def game_update(dt):
-    global foundGround
-
-    if player.can_move:
-        player.move(dt, keys)
-    
-    elif player.animation == "hook" and not keys[key.Z]:
-        if player.tongueOn:
-            # make the animation of tongue removing
-            player.tongueSize[1] -= 500*dt
-            player.pos[1] += 500*dt
-
-            if player.tongueSize[1] <= 50:
-                player.tongueOn = False
-                player.can_move = True
-                player.animation = None
-
-                return
-        else:
-            # fix flying bugs
-            player.animation = None
-    
-    foundGround = False
-    for gameobject in gameobjects:
-        if not gameobject:
-            continue
-
-        if gameobject.type == "hook":
-            # collision with hook ?
-            if player.checkCollide(gameobject, True) and player.tongueOn:
-                # redefine scrolling so that the object is in the center
-                scrolling[0] = - win_width//2 + gameobject.mid_pos(0)
-                scrolling[1] = ( - win_height//2 + gameobject.mid_pos(1) ) - win_height//10 # make it a bit higher
-
-                # forbide moving
-                player.can_move = False
-
-                # Enable animation bool
-                player.animation = "hook"
-            
-        elif gameobject.type in ["grass", "rock"]:
-            # collision with plateform ?
-            if player.checkGroundCollide(gameobject):
-                foundGround = True
-                player.speed_mod = 1
-                # player.pos[1] += 100*dt
-
-                gameobject.on_collision()
-
-                continue
-    
-    # not standing on a plateform ?
-    if not foundGround and not player.animation:
-        if player.tongueOn:
-            player.speed_mod = 0.2
-            player.pos[1] -= 50*dt
-        else:
-            player.speed_mod = 0.5
-            player.pos[1] -= 200*dt
-    
-    # out of the screen ?
-    if not player.animation or player.animation in ["rewind"]:
-        x_mod = 0
-        y_mod = 0
-        if player.sprite.y <= 100:
-            y_mod = -250
-        elif player.sprite.y >= win_height - 350:
-            y_mod = 250
-        if player.sprite.x <= 300:
-            x_mod = -250
-        elif player.sprite.x >= win_width - 300:
-            x_mod = 250
-        
-        if x_mod:
-            scrolling[0] += x_mod*dt
-        if y_mod:
-            scrolling[1] += y_mod*dt
-
-        # too low ?
-        if player.pos[1] < -1000:
-            player.pos[1] = 0
+    # update EVERYTHING about the player
+    player.game_update(dt, gameobjects, scrolling, keys, win_width, win_height)
     
     # label update
     coords_label.text = str([round(elm) for elm in player.pos])
@@ -207,6 +116,7 @@ def game_update(dt):
 # Schedule the update and draw functions
 pyglet.clock.schedule_interval(game_update, 1/120)
 pyglet.clock.schedule_interval(render_update, 1/120)
+pyglet.clock.schedule_interval(ground_music.play(), ground_music.duration)
 
 # Start the application
 pyglet.app.run()
